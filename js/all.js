a1980aa12243ac0c961a45da8182060fa2f27194
Jung-Yi, Tsai <jungyitsai@gmail.com>
2020-07-08 11:12:20 +0800

    [modify] modify the product data

--- js/all.js
+++ js/all.js
@@ -3,25 +3,29 @@ var app = new Vue({
     data: {
         product_list: [
             {
-                title: "Switch",
-                category: "掌上主機",
-                content: "商品敘述 - string",
+                title: "神戶一日遊",
+                category: "神戶",
+                content: "暢遊神戶五個景點",
                 description: "商品說明 - string",
-                imageUrl: "商品圖片 - string",
+                imageUrl: [
+                    "https://images.unsplash.com/photo-1561356048-1506f7e0f4b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                ],
                 enabled: true,
-                origin_price: 20000,
-                price: 9980,
+                origin_price: 1800,
+                price: 1400,
                 unit: "單位 - string",
             },
             {
-                title: "PS5 Wifi",
-                category: "主機",
-                content: "商品敘述 - string",
+                title: "大阪一日遊",
+                category: "大阪",
+                content: "暢遊大阪五個景點",
                 description: "商品說明 - string",
-                imageUrl: "商品圖片 - string",
+                imageUrl: [
+                    "https://images.unsplash.com/photo-1537153960513-344d9f2e8216?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                ],
                 enabled: false,
-                origin_price: 29999,
-                price: 9487,
+                origin_price: 3000,
+                price: 2500,
                 unit: "單位 - string",
             },
         ],
